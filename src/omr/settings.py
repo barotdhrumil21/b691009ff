b7883f2fa1ccc1f0102df4feb770f0e49b441f75
Ubuntu <danniel@OMRtest.ctzfmojhofwe5l0benjohgdzaa.sx.internal.cloudapp.net>
2019-11-17 04:20:57 +0000

    front end done

--- src/omr/settings.py
+++ src/omr/settings.py
@@ -123,12 +123,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATICFILES_DIRS = (
-    os.path.join("/home/danniel/b691009ff/src/functions/", "static/"),
-    '/var/www/static/',
-)
-STATIC_ROOT = "/home/danniel/b691009ff/src/static" #os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "Static_dev/static")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 LOGIN_URL = '/welcome/'
+
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
